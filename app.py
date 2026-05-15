de5f9b7e2b2c73b0ab5f6857febc76a365f2678c
nathalieteresa <159629950+nathalieteresa@users.noreply.github.com>
2026-05-15 10:25:47 -0400

    Update app.py

--- app.py
+++ app.py
@@ -187,7 +187,7 @@ st.markdown(f"""
 </style>
 """, unsafe_allow_html=True)
 
-GOOGLE_SHEET_CSV_URL = "https://docs.google.com/spreadsheets/d/1TDEk0iVLmS4506y5W1m5OSZynvbGEpF_NhkQXqMwspM/edit?gid=0#gid=0"
+GOOGLE_SHEET_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRlzu0foii8Px9Kajtdoa84Cy3rYy9VdCG3tBa-Hwt7rmisBrXF_x8dYdrn2RgHIhimS0YJNQFAoZVD/pub?gid=0&single=true&output=csv"
 
 GOOGLE_CACHE_FILE = "google_places_cache.csv"
 GOOGLE_CACHE_SHEET_CSV_URL = st.secrets.get("GOOGLE_CACHE_SHEET_CSV_URL", "")
